44370eaadac6a71c8427c1efd67befbeee801d16
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-10-02 19:44:14 +0100

    reformatted comments

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,4 @@
 /* This is the main script for deploying the FundMe contract.
- * The Main determinant is ethUsdPriceFeedAddress which is obtained by the if statement
  * If we are on a development chain, we deploy MockV3Aggregator, get the address and assign it to ethUsdPriceFeedAddress
  * If we are on a testnet, we get the address of the real AggregatorV3 contract and assign it to ethUsdPriceFeedAddress
  */
@@ -9,7 +8,7 @@ const { network } = require("hardhat")
 const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-    const { deploy, log } = deployments // same as deployments.deploy or deployments.log
+    const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
@@ -17,7 +16,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // const ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed
     let ethUsdPriceFeedAddress
     if (developmentChains.includes(network.name)) {
-        // getting the most recent deployment of MockV3Aggregator and assigning its address as the ethUsdPriceFeedAddress
+        // getting the most recent deployment of MockV3Aggregator
+        // Use ethers.getContract if you are calling a function from whatever contract you retrieve.
         let ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
@@ -27,9 +27,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: args, // put priceFeedAddress here
+        args: args,
         log: true,
-        waitConfirmations: network.config.blockConfirmation || 1, // change if you are working with a testnet
+        waitConfirmations: network.config.blockConfirmation || 1,
     })
     log(`FundMe deployed by ${deployer} at ${fundMe.address}`)
     log("********************************************************")
